4e0585381d01a7284b934b31922682c00f7e9bc1
Daan Breur <daanbreur@gmail.com>
2019-12-11 21:07:11 +0100

    2019 Day 4 Part 1 Finished

--- 2019/04/index.js
+++ 2019/04/index.js
@@ -1,22 +1,34 @@
 module.exports = main;
 
+let start = 357253,
+  end = 892942,
+  count = start,
+  amountPasswords = 0,
+  amountPasswordsPart2 = 0;
+
 function main() {
-  let start = 357253;
-  let end = 892942;
-  let count = 357253;
   while (count <= end) {
     let prevNumber = 0;
-    let skip = false;
-    let correct = false
+    let skip = false,
+      notSkip = false;
+
     for (const number of count.toString().split("")) {
       if (parseInt(number) < prevNumber) {
-        skip = true
-        break
+        skip = true;
+        break;
       }
-      prevNumber = number;
-      console.log()
+
+      if (parseInt(number) === prevNumber) {
+        notSkip = true;
+      }
+
+      prevNumber = parseInt(number);
     }
     count++;
-    if (skip || !correct) continue;
+    if (skip || !notSkip) continue;
+    amountPasswords++;
   }
+
+  console.log(`[Day 4 Part 1] Answer is: ${amountPasswords}`);
+  console.log(`[Day 4 Part 2] Answer is: ${amountPasswordsPart2}`);
 }
