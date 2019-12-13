d80f7a30b46e3dd8c4fb79e91b53dfda346b0b4b
Daan Breur <daanbreur@gmail.com>
2019-12-13 16:59:05 +0100

    //WIP [2019 Day 5 Part 2 ]

--- 2019/05/index.js
+++ 2019/05/index.js
@@ -4,46 +4,111 @@ module.exports = main;
 function main() {
   fs.readFile("./input.txt", "utf8", (err, data) => {
     let array = data.split(",");
-
-    console.log(
-      `[Day 5] Part 1: The first element's value is ${parseIntCode(array)}`
-    );
+    const firstPart = parseIntCode(array, "1");
+    console.log(`[Day 5] Part 1: The first element's value is ${firstPart}`);
+    const secondPart = parseIntCode(array, "5");
+    console.log(`[Day 5] Part 2: The second element's value is ${secondPart}`);
   });
 }
 
-function parseIntCode(array) {
-  let count = 0;
-  for (const opcode of array) {
-    if (count % 4 === 0) {
-      const firstElement = parseInt(array[count + 1]),
-        secondElement = parseInt(array[count + 2]),
-        overrideElement = parseInt(array[count + 3]);
-      let done = false;
-      switch (parseInt(opcode)) {
-        case 1:
-          array[overrideElement] = (
-            parseInt(array[firstElement]) + parseInt(array[secondElement])
-          ).toString();
-          break;
-        case 2:
-          array[overrideElement] = (
-            parseInt(array[firstElement]) * parseInt(array[secondElement])
-          ).toString();
-          break;
-        case 3:
-          array[firstElement] = "1";
-          break;
-        case 4:
-          console.log(
-            `[Day 5] Part 1: OUTPUT OF THE INTCODE 4 IS ${array[firstElement]}`
-          );
-          break;
-        case 99:
-          done = true;
-          break;
-      }
-      if (done) return array[0];
+function parseIntCode(array, input) {
+  for (let i = 0; i < array.length; i++) {
+    let opcode = array[i];
+    while (opcode.split("").length < 5) opcode = `0${opcode}`;
+    const parameterMode = opcode.split(""),
+      firstParameter = parseInt(array[i + 1]),
+      secondParameter = parseInt(array[i + 2]),
+      overrideParameter = parseInt(array[i + 3]);
+    let done = false;
+    switch (parseInt(opcode.substr(opcode.length - 2, opcode.length - 1))) {
+      case 1:
+        array[overrideParameter] = (
+          (parameterMode[2] === "0"
+            ? parseInt(array[firstParameter])
+            : firstParameter) +
+          (parameterMode[1] === "0"
+            ? parseInt(array[secondParameter])
+            : secondParameter)
+        ).toString();
+        i += 3;
+        break;
+      case 2:
+        array[overrideParameter] = (
+          (parameterMode[2] === "0"
+            ? parseInt(array[firstParameter])
+            : firstParameter) *
+          (parameterMode[1] === "0"
+            ? parseInt(array[secondParameter])
+            : secondParameter)
+        ).toString();
+        i += 3;
+        break;
+      case 3:
+        array[firstParameter] = input;
+        i += 1;
+        break;
+      case 4:
+        const returnValue =
+          parameterMode[2] === "0"
+            ? parseInt(array[firstParameter])
+            : firstParameter;
+        if (returnValue !== 0) return returnValue;
+        i += 1;
+        break;
+      case 5:
+        if (
+          (parameterMode[2] === "0"
+            ? parseInt(array[firstParameter])
+            : firstParameter) !== 0
+        )
+          i =
+            parameterMode[1] === "0"
+              ? parseInt(array[secondParameter])
+              : secondParameter;
+        else i += 2;
+        break;
+      case 6:
+        if (
+          (parameterMode[2] === "0"
+            ? parseInt(array[firstParameter])
+            : firstParameter) === 0
+        )
+          i =
+            parameterMode[1] === "0"
+              ? parseInt(array[secondParameter])
+              : secondParameter;
+        else i += 2;
+        break;
+      case 7:
+        if (
+          (parameterMode[2] === "0"
+            ? parseInt(array[firstParameter])
+            : firstParameter) <
+          (parameterMode[1] === "0"
+            ? parseInt(array[secondParameter])
+            : secondParameter)
+        )
+          array[overrideParameter] = "1";
+        else array[overrideParameter] = "0";
+        i += 3;
+        break;
+      case 8:
+        if (
+          (parameterMode[2] === "0"
+            ? parseInt(array[firstParameter])
+            : firstParameter) ==
+          (parameterMode[1] === "0"
+            ? parseInt(array[secondParameter])
+            : secondParameter)
+        )
+          array[overrideParameter] = "1";
+        else array[overrideParameter] = "0";
+        i += 3;
+        break;
+      case 99:
+        done = true;
+        break;
     }
-    count++;
+    if (done) return array[0];
   }
 }
