608e293f2697ae49dbcc9b7bb02612b3b22bbef9
Daan Breur <daanbreur@gmail.com>
2019-12-15 04:04:33 +0100

    [2019 Day 8 Part 2] Part 2 FINISHEDD :D

--- 2019/08/index.js
+++ 2019/08/index.js
@@ -27,12 +27,34 @@ function main() {
       `[2019 Day 8 Part 1] The First Elements value is ${numbersInLayer.one *
         numbersInLayer.two}`
     );
-    console.log(`[2019 Day 8 Part 2] The Second Elements value is `);
+    console.log(`[2019 Day 8 Part 2] The Image is\n ${decodeImage(layers)} `);
   });
 }
 
 function decodeImage(layers) {
+  let result = "";
+  let objectObject = {};
   for (const layer of layers) {
+    let count = 0;
     const rows = layer.match(new RegExp(".{1," + width + "}", "g"));
+    for (const row of rows) {
+      const pixelsRow = row.split("");
+      for (const pixel of pixelsRow) {
+        if (!objectObject[count] || objectObject[count] === "2") {
+          if (pixel === "1") objectObject[count] = "█";
+          else if (pixel === "0") objectObject[count] = " ";
+          else objectObject[count] = pixel;
+        }
+        count++;
+      }
+      objectObject[count] = "\n";
+      count++;
+    }
   }
+
+  for (const pixel of Object.values(objectObject)) {
+    result += pixel;
+  }
+
+  return result;
 }
