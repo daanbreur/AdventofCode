dec8a1888e6d52691d2a97ac010432d7fcb017a3
Daan Breur <daanbreur@gmail.com>
2020-12-01 07:56:37 +0100

    [Python][2020] Add code for day 01

---
+++ 2020/01/main.py
@@ -0,0 +1,17 @@
+with open('input.txt') as file:
+  contents = list(map(int, file.read().splitlines()))
+
+
+def part1():
+  for i in contents:
+    for j in contents:
+      if i + j == 2020: return i * j
+
+def part2():
+  for i in contents:
+    for j in contents:
+      for n in contents:
+        if i + j + n == 2020: return i * j * n
+
+print('Output Part 1: {} '.format(part1()))
+print('Output Part 2: {} '.format(part2()))
