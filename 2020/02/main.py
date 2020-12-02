efae40b5b7e20be3a55e5fca6f01504ccbcd41bf
Daan Breur <daanbreur@gmail.com>
2020-12-02 08:24:59 +0100

    [Python][2020] Part02 for Day02

--- 2020/02/main.py
+++ 2020/02/main.py
@@ -14,7 +14,15 @@ def part1():
   return count
 
 def part2():
-  pass
+  count=0
+  for line in contents:
+    params = line.split(' ')
+    charCount = params[2].count(params[1][:-1])
+    [pos1I,pos2I] = list(map(int, params[0].split('-')))
+    print(params, charCount, pos1I, pos2I)
+    if (params[2][pos1I-1] == params[1][:-1]) != (params[2][pos2I-1] == params[1][:-1]):
+      count += 1
+  return count
 
 print('Day02 Part 1: {} '.format(part1()))
 print('Day02 Part 2: {} '.format(part2()))
