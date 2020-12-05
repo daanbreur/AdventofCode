7ef7a2ae466a3b79818e0eeda73967edc8ef0172
Daan Breur <daanbreur@gmail.com>
2020-12-05 19:57:38 +0100

    [Python][2020] Optimize day03, wraparound string

--- 2020/03/main.py
+++ 2020/03/main.py
@@ -1,5 +1,6 @@
+import math
 with open('input.txt') as file:
-  contents = list(map( lambda x: list(x * 250), file.read().splitlines()))
+  contents = file.read().splitlines()
 
 
 def part1():
@@ -8,6 +9,7 @@ def part1():
     #print( f'X: {x} ; Y: {y} ; ICON: {contents[y][x]}' )
     if contents[y][x] == '#': count += 1
     x += 3; y += 1
+    if (x >= len(contents[0])): x -= math.floor(x / len(contents[0])) * len(contents[0])
   return count
 
 def part2():
@@ -18,6 +20,7 @@ def part2():
       #print( f'X: {x} ; Y: {y} ; ICON: {contents[y][x]}' )
       if contents[y][x] == '#': count += 1
       x += slope[0]; y += slope[1]
+      if (x >= len(contents[0])): x -= math.floor(x / len(contents[0])) * len(contents[0])
     #print( f'SLOPE: {slope} ; COUNT: {count}' )
     treeCount *= count
   return treeCount
