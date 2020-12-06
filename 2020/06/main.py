da52d7268975fb57d3b4053602af997c8e59b25a
Daan Breur <daanbreur@gmail.com>
2020-12-06 10:41:34 +0100

    [Python][2020] code for day6 part2 + update stars

--- 2020/06/main.py
+++ 2020/06/main.py
@@ -9,7 +9,16 @@ def part1():
   return count
 
 def part2():
-  pass
+  count = 0
+  for answerGroup in answerGroups:
+    answerGroup = [ i for i in answerGroup.replace('\n',' ').strip().split(' ')]
+    for char in answerGroup[0]:
+      all = True
+      for i in answerGroup[1:len(answerGroup)]:
+        if char not in i:
+          all = False
+      if all: count += 1
+  return count
 
 
 print('Day04 Part 1: {} '.format(part1()))
