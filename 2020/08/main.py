157b1881d7eec3dabfdac68d7edfe2a677525220
Daan Breur <daanbreur@gmail.com>
2020-12-08 12:06:36 +0100

    [Python][2020] code for day08 part2

--- 2020/08/main.py
+++ 2020/08/main.py
@@ -1,6 +1,9 @@
 with open('input.txt') as file:
   instructions = file.read().splitlines()
 
+def getSpacer(space, lengthStr):
+  return " "*(space-lengthStr)
+
 def part1():
   accumulator, index = 0, 0
   history = []
@@ -11,7 +14,7 @@ def part1():
     opcode = instructions[index].split(' ')[0]
     arg = instructions[index].split(' ')[1]
     history.append(index)
-    print(f"Instruction: {instructions[index]} | OPCode: {opcode} Argument: {arg}")
+    #print(f"Instruction: {instructions[index]} | OPCode: {opcode} Argument: {arg}")
     if opcode == "acc": accumulator += int(arg)
     if opcode == "jmp":
       index += int(arg)
@@ -19,7 +22,33 @@ def part1():
 
 
 def part2():
-  pass
+  accumulator = 0
+  for i in range(len(instructions)):
+    temp=instructions.copy()
+    opcode, arg = temp[i].split(' ')
+    if not (opcode == "jmp" or opcode == "nop"): continue
+    if opcode == "jmp": temp[i] = f"nop {arg}"  
+    elif opcode == "nop": temp[i] = f"jmp {arg}"
+    index = 0
+    history = []
+    stopped = False
+    while not stopped and index < len(temp):
+      if index in history:
+        accumulator = 0
+        stopped = True
+        break
+
+      opcode = temp[index].split(' ')[0]
+      arg = temp[index].split(' ')[1]
+      history.append(index)
+      print(f"Instruction: {temp[index]}{getSpacer(10, len(temp[index]))}| OPCode: {opcode}{getSpacer(6, len(opcode))}Argument: {arg}")
+      if opcode == "acc": accumulator += int(arg)
+      if opcode == "jmp":
+        index += int(arg)
+      else: index += 1
+    if not stopped:
+      return accumulator
+    
 
 print('Day08 Part 1: {} '.format(part1()))
 print('Day08 Part 2: {} '.format(part2()))
