30ae1be7b4d33ee1b03429a9f9a5c6a4897017e3
Daan Breur <daanbreur@gmail.com>
2020-12-09 11:28:04 +0100

    [Python][2020] code for day09 part2

--- 2020/09/main.py
+++ 2020/09/main.py
@@ -1,5 +1,5 @@
 with open('input.txt') as file:
-  contents = file.read().splitlines()
+  contents = list(map(int, file.read().splitlines()))
 
 def part1():
   for i in range(25,len(contents)):
@@ -8,13 +8,21 @@ def part1():
     for n in currentPreamble:
       for m in currentPreamble:
         if int(n) + int(m) == int(contents[i]): notValid = False
-    if notValid: return int(contents[i])
+    if notValid: 
+      return contents[i]
         
     
 
 def part2():
-  pass
-    
+  mainList = []
+  target = part1
+  for i in range(len(contents)):
+    thisList = contents[i:].copy()
+    for j in range(len(thisList)):
+      mainList.append(thisList[j])
+      if (target == sum(mainList)) and (len(mainList) > 1): return min(mainList) + max(mainList)
+      elif target < sum(mainList): mainList.clear()
+
 
 print('Day09 Part 1: {} '.format(part1()))
 print('Day09 Part 2: {} '.format(part2()))
