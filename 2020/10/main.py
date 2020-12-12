e6bac314a0c1d6cd4b63638476a510b2f2e4a615
Daan Breur <daanbreur@gmail.com>
2020-12-12 16:43:26 +0100

    [Python][2020] code for day10 part2

--- 2020/10/main.py
+++ 2020/10/main.py
@@ -15,7 +15,13 @@ def part1():
   return count1 * count3
 
 def part2():
-  pass
+  data = contents.copy()
+  data.sort() 
+  data.append(data[-1]+1)
+  temp = {0: 1}
+  for r in data:
+    temp[r] = temp.get(r-3,0) + temp.get(r-2,0) + temp.get(r-1,0)
+  return temp[data[-1]]
 
 start_time = time.time_ns()
 print('\033[38;2;60;179;113mDay10 Part 1: {} \033[0m'.format(part1()))
