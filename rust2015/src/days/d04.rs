39ca9c8f874e6b4950554f8f3a8a8143d3374093
Daan Breur <git@daanbreur.systems>
2022-12-04 22:31:35 +0100

    day 4 year 2015 honestly just ended up stealing most from some article

--- rust2015/src/days/d04.rs
+++ rust2015/src/days/d04.rs
@@ -1,32 +1,74 @@
+extern crate crypto;
+
+use crypto::md5::Md5;
+use crypto::digest::Digest;
+
+use crate::dprintln;
+
 use super::{Answer, Day, DayImpl};
 
 const CURRENT_DAY: u8 = 4;
 
-type Data = Vec<u64>;
+type Data = String;
 impl DayImpl<Data> for Day<CURRENT_DAY> {
     fn init_test() -> (Self, Data) {
         Self::init(&include_str!("test_inputs/test04.txt").to_owned())
     }
 
     fn expected_results() -> (Answer, Answer) {
-        (Answer::Number(0), Answer::Number(0))
+        (Answer::Number(609043), Answer::Number(0))
     }
 
     fn init(input: &str) -> (Self, Data) {
         (
             Self {},
             input
-                .lines()
-                .map(|v| v.parse::<u64>().expect("error while parsing input."))
-                .collect(),
+                .trim()
+                .to_owned(),
         )
     }
 
     fn one(&self, data: &mut Data) -> Answer {
-        Answer::Number(data.len() as u64)
+        let secret: &[u8] = data.as_bytes();
+        let mut hasher = Md5::new();
+        let mut answer: u64 = 0;
+
+        for i in 0..u64::MAX {
+            hasher.input(secret);
+            hasher.input(i.to_string().as_bytes());
+
+            let output = hasher.result_str();
+
+            if output.starts_with("00000") {
+                answer = i;
+                break;
+            }
+
+            hasher.reset();
+        }
+
+        return Answer::Number(answer as u64)
     }
 
     fn two(&self, data: &mut Data) -> Answer {
-        Answer::Number(data.len() as u64)
+        let secret: &[u8] = data.as_bytes();
+        let mut hasher = Md5::new();
+        let mut answer: u64 = 0;
+
+        for i in 0..u64::MAX {
+            hasher.input(secret);
+            hasher.input(i.to_string().as_bytes());
+
+            let output = hasher.result_str();
+
+            if output.starts_with("000000") {
+                answer = i;
+                break;
+            }
+
+            hasher.reset();
+        }
+
+        return Answer::Number(answer as u64)
     }
 }
