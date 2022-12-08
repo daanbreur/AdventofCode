8e5c69761e43a8a4087467fa2adf8f478cfd91f4
Daan Breur <git@daanbreur.systems>
2022-12-08 22:03:06 +0100

    Day 7!!!

--- rust2022/src/days/d07.rs
+++ rust2022/src/days/d07.rs
@@ -4,163 +4,59 @@ use super::{Answer, Day, DayImpl};
 
 const CURRENT_DAY: u8 = 7;
 
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-enum FilesystemItem {
-    Directory(Directory),
-    File(File),
-}
-
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-struct File {
-    name: String,
-    size: u64,
-}
-
-impl File {
-    pub fn get_size(&self) -> u64 {
-        self.size
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Hash, Clone)]
-pub struct Directory {
-    name: String,
-    parent: Option<*mut Directory>,
-    children: Vec<FilesystemItem>,
-}
-
-impl Directory {
-    pub fn add_children(&mut self, item: FilesystemItem) {
-        self.children.push(item);
-    }
-
-    pub fn get_size(&self) -> u64 {
-        let mut size: u64 = 0;
-
-        for item in self.children.iter() {
-            match item {
-                FilesystemItem::Directory(data) => {
-                    size += data.get_size();
-                },
-                FilesystemItem::File(data) => {
-                    size += data.get_size();
-                }
-            }
-        }
-
-        size
-    }
-
-    pub fn get_directory(&mut self, name: String) -> Option<&mut Directory> {
-        for item in &mut self.children {
-            match item {
-                FilesystemItem::Directory(data) => {
-                    if data.name == name {
-                        return Some(data);
-                    }
-                },
-                FilesystemItem::File(_data) => {}
-            }
-        }
-
-        return None
-    }
-
-    pub fn get_parent(&self) -> Option<&Directory> {
-        unsafe { self.parent.unwrap().as_ref() }
-    }
-
-    pub fn get_mut_parent(&mut self) -> Option<&mut Directory> {
-        unsafe { self.parent.unwrap().as_mut() }
-    }
-}
-
-type Data = Directory;
+type Data = Vec<String>;
 impl DayImpl<Data> for Day<CURRENT_DAY> {
     fn init_test() -> (Self, Data) {
         Self::init(&include_str!("test_inputs/test07.txt").to_owned())
     }
 
     fn expected_results() -> (Answer, Answer) {
-        (Answer::Number(95437), Answer::Number(0))
+        (Answer::Number(95437), Answer::Number(24933642))
     }
 
     fn init(input: &str) -> (Self, Data) {
-        
-        let mut root_dir: Directory = Directory {
-            parent: None,
-            name: String::from(""),
-            children: Vec::new()
-        };
-
-        let mut current_dir: &mut Directory = &mut root_dir;
-        let mut is_ls_response: bool = false;
-
-        for line in input.lines() {
-            if line.starts_with("$") {
-                is_ls_response = false;
-                let command = line.to_string().replace("$ ", "");
-                let mut split_command = command.split(" ");
-                match split_command.next().unwrap() {
-                    "cd" => {
-                        match split_command.next().unwrap() {
-                            "/" => {
-                                current_dir = &mut root_dir;
-                            },
-                            ".." => {
-                                current_dir = current_dir.get_mut_parent().unwrap();
-                            },
-                            dir => {
-                                current_dir = current_dir.get_directory(dir.to_string()).expect("error while parsing input. directory is not defined.");
-                            },
-                        }
-                    },
-                    "ls" => {
-                        is_ls_response = true;
-                    },
-                    _ => {},
-                }
-            }
-
-            if is_ls_response {
-                let mut data = line.split(" ");
-                let size = data.next().unwrap();
-                let name = data.next().unwrap();
-                let item;
-
-                if size == "dir" {
-                    item = FilesystemItem::Directory(Directory { parent: Some(&mut *current_dir), name: String::from(name), children: Vec::new() });
-                } else {
-                    item = FilesystemItem::File(File { name: String::from(name), size: size.parse::<u64>().expect("error while parsing input.") });
-                }
-                current_dir.add_children(item);
-            }
-        }
-
-        dprintln!("{:?}", root_dir);
-        dprintln!("{:?}", root_dir.get_size());
-
         (
             Self {},
-            root_dir,
+            input.lines().map(|x| x.to_owned()).collect::<Vec<String>>(),
         )
     }
 
     fn one(&self, data: &mut Data) -> Answer {
         let mut total_size: u64 = 0;
+        let mut stack: Vec<u64> = vec![0];
 
-        for item in &data.children {
-            match item {
-                FilesystemItem::Directory(data) => {
-                    if data.get_size() < 100_000 {
-                        total_size += data.get_size();
+        for line in data {
+            if line.starts_with("$") {
+                let command = line.to_string().replace("$ ", "");
+                let mut split_command = command.split(" ");
+                match split_command.next().unwrap() {
+                    "cd" => {
+                        match split_command.next().unwrap() {
+                            "/" => {},
+                            ".." => {
+                                let current_dir = stack.pop().unwrap();
+                                if current_dir < 100_000 {
+                                    total_size += current_dir;
+                                }
+                                if let Some(new_dir) = stack.last_mut() {
+                                    *new_dir += current_dir;
+                                }
+                            },
+                            _dir => {
+                                stack.push(0);
+                            },
+                        }
+                    },
+                    _ => {},
+                }
+            } else {
+                if let Some(current_dir) = line.split_once(" ") {
+                    if let Ok(size) = current_dir.0.parse::<u64>() {
+                        if let Some(directory) = stack.last_mut() {
+                            *directory += size;
+                        }
                     }
-                },
-                FilesystemItem::File(_data) => {
-                    // if data.get_size() < 100_000 {
-                    //     total_size += data.get_size();
-                    // }
-                },
+                }
             }
         }
 
@@ -168,6 +64,62 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
     }
 
     fn two(&self, data: &mut Data) -> Answer {
-        Answer::Number(0 as u64)
+        let mut stack: Vec<i64> = vec![0];
+        let mut directory_sizes: Vec<i64> = Vec::new();
+
+        for line in data {
+            if line.starts_with("$") {
+                let command = line.to_string().replace("$ ", "");
+                let mut split_command = command.split(" ");
+                match split_command.next().unwrap() {
+                    "cd" => {
+                        match split_command.next().unwrap() {
+                            "/" => {},
+                            ".." => {
+                                let current_dir = stack.pop().unwrap();
+                                if let Some(new_dir) = stack.last_mut() {
+                                    *new_dir += current_dir;
+                                }
+                                directory_sizes.push(current_dir);
+                            },
+                            _dir => {
+                                stack.push(0);
+                            },
+                        }
+                    },
+                    _ => {},
+                }
+            } else {
+                if let Some(current_dir) = line.split_once(" ") {
+                    if let Ok(size) = current_dir.0.parse::<i64>() {
+                        if let Some(directory) = stack.last_mut() {
+                            *directory += size;
+                        }
+                    }
+                }
+            }
+        }
+
+        while stack.len() > 0 { 
+            let size = stack.pop().unwrap();
+            directory_sizes.push(size);
+            if stack.len() > 0 {
+                if let Some(directory) = stack.last_mut() {
+                    *directory += size;
+                }
+            }
+        }
+
+        let space_remaining = 70_000_000 - directory_sizes.last().unwrap();
+        let space_required = 30_000_000 - space_remaining;
+
+        let delete_size: i64 = directory_sizes
+            .into_iter()
+            .filter(|x| x >= &space_required)
+            .min()
+            .unwrap();
+            
+
+        Answer::Number(delete_size as u64)
     }
 }
