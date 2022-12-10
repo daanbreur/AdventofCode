26504718e32f79c1873194a5141b65f12a168076
Daan Breur <git@daanbreur.systems>
2022-12-10 18:27:34 +0100

    Day 10 Part 1 :/

--- rust2022/src/days/d10.rs
+++ rust2022/src/days/d10.rs
@@ -1,15 +1,17 @@
+use crate::{dprintln, vprintln, dprint, vprint};
+
 use super::{Answer, Day, DayImpl};
 
 const CURRENT_DAY: u8 = 10;
 
-type Data = Vec<u64>;
+type Data = Vec<String>;
 impl DayImpl<Data> for Day<CURRENT_DAY> {
     fn init_test() -> (Self, Data) {
         Self::init(&include_str!("test_inputs/test10.txt").to_owned())
     }
 
     fn expected_results() -> (Answer, Answer) {
-        (Answer::Number(0), Answer::Number(0))
+        (Answer::Number(13140), Answer::Number(0))
     }
 
     fn init(input: &str) -> (Self, Data) {
@@ -17,16 +19,83 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
             Self {},
             input
                 .lines()
-                .map(|v| v.parse::<u64>().expect("error while parsing input."))
+                .map(|x| x.to_string())
                 .collect(),
         )
     }
 
     fn one(&self, data: &mut Data) -> Answer {
-        Answer::Number(data.len() as u64)
+        let mut answer: i64 = 0;
+        let mut instuction_counter = 0;
+        let mut register_x: i64 = 1;
+
+        for line in data {
+            let mut instruction = line.split(" ");
+            let mut inst = instruction.next().unwrap();
+            let mut data = instruction.next();
+            vprintln!("TAPE: {:?} - INSTRUCTION: {:?} - DATA: {:?}", line, inst, data);
+            match inst {
+                "noop" => {
+                    vprint!("oldIC: {:?} - oldRX: {:?} ", instuction_counter, register_x);
+                    instuction_counter += 1;
+                    if instuction_counter == 20 || instuction_counter == 60 || instuction_counter == 100 || instuction_counter == 140 || instuction_counter == 180 || instuction_counter == 220 {
+                        answer += instuction_counter*register_x;
+                    }
+                    vprintln!(" |  IC: {:?} - RX: {:?}", instuction_counter, register_x);
+                },
+                "addx" => {
+                    instuction_counter += 1;
+                    if instuction_counter == 20 || instuction_counter == 60 || instuction_counter == 100 || instuction_counter == 140 || instuction_counter == 180 || instuction_counter == 220 {
+                        answer += instuction_counter*register_x;
+                    }
+                    instuction_counter += 1;
+                    if instuction_counter == 20 || instuction_counter == 60 || instuction_counter == 100 || instuction_counter == 140 || instuction_counter == 180 || instuction_counter == 220 {
+                        answer += instuction_counter*register_x;
+                    }
+                    register_x += data.unwrap().parse::<i64>().unwrap();
+                },
+                _ => unreachable!(),
+            }
+        }
+
+        Answer::Number(answer as u64)
     }
 
     fn two(&self, data: &mut Data) -> Answer {
-        Answer::Number(data.len() as u64)
+        let mut instuction_counter = 0;
+        let mut crt_counter = 0;
+        let mut register_x: i64 = 1;
+
+        for line in data {
+            let mut instruction = line.split(" ");
+            let mut inst = instruction.next().unwrap();
+            let mut data = instruction.next();
+            vprintln!("TAPE: {:?} - INSTRUCTION: {:?} - DATA: {:?}", line, inst, data);
+            match inst {
+                "noop" => {
+                    vprint!("oldCRT: {:?} - oldIC: {:?} - oldRX: {:?} ", crt_counter, instuction_counter, register_x);
+                    instuction_counter += 1;
+
+                    vprintln!(" |  CRT: {:?} - IC: {:?} - RX: {:?}", crt_counter, instuction_counter, register_x);
+                },
+                "addx" => {
+                    instuction_counter += 1;
+                    if instuction_counter == 20 || instuction_counter == 60 || instuction_counter == 100 || instuction_counter == 140 || instuction_counter == 180 || instuction_counter == 220 {
+                    }
+                    instuction_counter += 1;
+                    if instuction_counter == 20 || instuction_counter == 60 || instuction_counter == 100 || instuction_counter == 140 || instuction_counter == 180 || instuction_counter == 220 {
+                        
+                    }
+                    register_x += data.unwrap().parse::<i64>().unwrap();
+                },
+                _ => unreachable!(),
+            }
+        }
+
+
+        Answer::String(String::new())
     }
 }
+
+//█
+// 
