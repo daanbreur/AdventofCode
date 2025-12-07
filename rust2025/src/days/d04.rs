0dd2aa06124d3f1239ab26c3a492156ab9eb47ba
Daan Breur <git@daanbreur.systems>
2025-12-07 12:28:26 +0100

    2025 day4 part2

--- rust2025/src/days/d04.rs
+++ rust2025/src/days/d04.rs
@@ -11,7 +11,7 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
     }
 
     fn expected_results() -> (Answer, Answer) {
-        (Answer::Number(13), Answer::Number(0))
+        (Answer::Number(13), Answer::Number(43))
     }
 
     fn init(input: &str) -> (Self, Data) {
@@ -72,6 +72,54 @@ impl DayImpl<Data> for Day<CURRENT_DAY> {
     }
 
     fn two(&self, data: &mut Data) -> Answer {
-        Answer::Number(data.len() as u64)
+        dprintln!("input data: {:?}", data);
+
+        let mut count: u64 = 0;
+
+        let (w, h) = (data[0].len(), data.len());
+
+        loop {
+            let mut to_remove: Vec<(usize, usize)> = Vec::new();
+
+            for i in 0..h {
+                for j in 0..w {
+                    if !data[i][j] { continue; }
+
+                    let mut neighbors = 0;
+
+                    for di in -1..=1 {
+                        for dj in -1..=1 {
+                            if di == 0 && dj == 0 { continue; }
+
+                            let neighbor_i = i as isize + di;
+                            let neighbor_j = j as isize + dj;
+
+                            if neighbor_i < 0 || neighbor_i >= h as isize || neighbor_j < 0 || neighbor_j >= w as isize {
+                                continue;
+                            }
+
+                            if data[neighbor_i as usize][neighbor_j as usize] {
+                                neighbors += 1;
+                            }
+                        }
+                    }
+
+                    if neighbors < 4 {
+                        to_remove.push((i, j));
+                    }
+                }
+            }
+
+            if to_remove.is_empty() {
+                break;
+            }
+
+            for (i, j) in to_remove {
+                data[i][j] = false;
+                count += 1;
+            }
+        }
+        
+        Answer::Number(count)
     }
 }
